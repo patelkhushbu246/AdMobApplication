1db8cfee44724801bea62ffd6956d03bb0984cbb
patelkhushbu246 <khushbupatel2497@yahoo.com>
2021-08-23 11:44:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/admobapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.admobapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
